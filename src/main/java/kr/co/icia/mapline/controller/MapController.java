f4df0469430f52d353852d353243644f7e62c1f0
anjaemun <ajm0111@naver.com>
2024-01-25 15:42:14 +0900

    키워드 검색 마커표시

--- src/main/java/kr/co/icia/mapline/controller/MapController.java
+++ src/main/java/kr/co/icia/mapline/controller/MapController.java
@@ -89,5 +89,36 @@ public class MapController {
 		}
 		return "map/marker";
 	}
+	/**
+     * 키워드로 검색하기
+     *
+     * @param keyword 검색어
+     * @param x       중심좌표 x
+     * @param y       중심좌표 y
+     * @param model   html파일에 값을 전달해주는 객체
+     * @return html 파일위치
+     */
+    @GetMapping("/map/keyword")
+    public String getKeyword(@RequestParam(required = false) String keyword, //keyword를 입력받음
+                             @RequestParam(required = false) String x, //x좌표를 입력받음
+                             @RequestParam(required = false) String y, Model model) throws IOException, InterruptedException { //y좌표를 입력받음
+        if (keyword != null && !keyword.isEmpty() &&
+            x != null && !x.isEmpty() &&
+            y != null && !y.isEmpty()) { //keyword, x, y값이 모두 입력되었을 때 실행
+            List<KakaoApiUtil.Pharmacy> pharmacyList = KakaoApiUtil.getPointsByKeyword(keyword, x, y); //keyword, x, y값을 getPointsByKeyword에 넣어서 반환되는 Pharmacy로 구성된 List를 저장
+            int cnt = 0; //pharmacyList의 크기를 저장할 변수
+            assert pharmacyList != null;//pharmacyList가 null이 아닐 때 실행
+            for (KakaoApiUtil.Pharmacy pharmacy : pharmacyList) { //pharmacyList의 크기만큼 반복
+                cnt++; //pharmacyList의 크기를 저장
+            }
+            System.out.println(cnt); //pharmacyList의 크기를 출력
+            String pharmacyListJson = new ObjectMapper().writer().writeValueAsString(pharmacyList); //pharmacyList를 json형태로 변환
+            model.addAttribute("pharmacyList", pharmacyListJson); //html로 보냄
+            System.out.println("실행됨"); //실행됐는지 확인
+        }
+        return "map/keyword"; //html 파일위치
+    }
+
+
 
 }
